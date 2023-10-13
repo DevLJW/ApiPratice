be5ac1c28e85d1ef2571386f4c661b5327cacaec
DevLJW <jaewookv123@gmail.com>
2023-10-13 13:31:18 +0900

    dto product input add

--- backendPratice/section10/10-03-typeorm-crud/src/APIS/products/dto/create-product.input.ts
+++ backendPratice/section10/10-03-typeorm-crud/src/APIS/products/dto/create-product.input.ts
@@ -1,5 +1,6 @@
 import { Int } from '@nestjs/graphql';
 import { Field, InputType, ObjectType } from '@nestjs/graphql/dist/decorators';
+
 //  타입스크립트에서 작성한 코드를 Grapql형식으로 변환을 해야하기때문에
 //  데코레이션을 작성한다.
 //  @InputType,@Field() 그래프큐엘 타입으로 변환을 해줘야 그래프큐엘에서 사용이 가능하다.
@@ -9,6 +10,7 @@ export class CreateProductInput {
     name: string;
     @Field(() => String)
     description: string;
+   
     @Field(() => Int)
     price: number;
 }
